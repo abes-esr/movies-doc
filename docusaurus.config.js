a51f8852e1ee8553ebe125108e2f87aeabe74328
Mathis EON <60341438+eonm-abes@users.noreply.github.com>
2022-12-15 15:40:36 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Documentation Movies',
   tagline: 'Modélisation de la vie des établissements de l\'ESR',
-  //url: 'https://abes-esr.github.io/movies-doc/',
+  url: 'https://abes-esr.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
