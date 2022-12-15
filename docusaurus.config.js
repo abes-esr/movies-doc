457d8ef0325474196c7db504fbb9fcedacc48fde
Mathis EON <60341438+eonm-abes@users.noreply.github.com>
2022-12-15 15:30:18 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Documentation Movies',
   tagline: 'Modélisation de la vie des établissements de l\'ESR',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://abes-esr.github.io/movies-doc/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'abes-esr', // Usually your GitHub org/user name.
   projectName: 'movies-doc', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
