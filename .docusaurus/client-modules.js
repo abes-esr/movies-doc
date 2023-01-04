797333d9c26028e0f1bf2e83fd5e244ebc15f8dc
Mathis EON <eon@abes.fr>
2023-01-04 16:45:53 +0100

    WIP

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/mnt/c/Users/eon/Dev/movies-doc/node_modules/infima/dist/css/default/default.css'),
-  require('/mnt/c/Users/eon/Dev/movies-doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/mnt/c/Users/eon/Dev/movies-doc/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/mnt/c/Users/eon/Dev/movies-doc/src/css/custom.css'),
-];
